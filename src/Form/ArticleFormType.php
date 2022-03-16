d0af38a29d15c275b40f3b492e3af9f75b9b651a
PhillTartari92 <phillipevianna@hotmail.fr>
2022-03-16 17:01:00 +0100

    Finish CRUD + dashboard+ form constraints

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,15 +4,16 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\Categorie;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
-
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleFormType extends AbstractType
 {
@@ -20,24 +21,58 @@ class ArticleFormType extends AbstractType
     {
         $builder
             ->add('title', TextType::class, [
-                'label' => 'Titre de l\'article',
+                'label'        => 'Titre de l\'article',
+                'constraints'  => [
+                    new NotBlank([
+                        'message'    => "Ce champ ne peut être vide." 
+                    ]),
+                    new Length([
+                        'min'        => 5 ,
+                        'max'        => 255,
+                        'minMessage' => "Le nombre de caractères minimal est {{ limit }} : votre titre contient {{ value }} caractères",
+                        'maxMessage' => "Le nombre de caractères maximal est {{ limit }} : votre titre contient {{ value }} caractères",
+                    ])
+                ]
             ])
             ->add('subtitle', TextType::class, [
-                'label' => 'Sous-titre',
+                'label'        => 'Sous-titre',
+                'constraints'  => [
+                    new NotBlank([
+                        'message'    => "Ce champ ne peut être vide." 
+                    ]),
+                    new Length([
+                        'min'        => 5 ,
+                        'max'        => 255,
+                        'minMessage' => " Votre titre est trop court. Le nombre de caractères minimal est {{ limit }}",
+                        'maxMessage' => " Votre titre est trop long . Le nombre de caractères maximal est {{ limit }}",
+                    ])
+                ]
             ])
             ->add('content', TextareaType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'Ici le contenu de l\'article'
+                'label'        => false,
+                'attr'         => [
+                   'placeholder'      => 'Ici le contenu de l\'article'
                 ],
             ])
             ->add('category', EntityType::class, [
-                'class' => Categorie::class,
+                'class'        => Categorie::class,
                 'choice_label' => 'name',
-                'label' => 'Choisissez une catégorie'
+                'label'        => 'Choisissez une catégorie'
             ])
             ->add('photo', FileType::class, [
-                'label' => 'Photo d\'illustration', 
+                'label'        => 'Photo d\'illustration', 
+                //'data_class' => permet de paramètrer le type de classe de données à null.
+                //Par défault data_class = File
+                'data_class'   => null,
+                'attr'         => [
+                  'data-default-file'    => $options['photo']     
+                ],
+                  'constraints'          => [
+                    new Image([
+                      'mimeTypes'        => ['image/jpeg', 'image/png'],
+                      'mimeTypesMessage' => "Les types de photo autorisés sont : .jpg et . png",
+                    ]),
+                ],
             ])
         ;
     }
@@ -45,7 +80,11 @@ class ArticleFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Article::class,
+            'data_class'        => Article::class,
+            //'allow_file_upload' => permet d'autoriser les upload de fichier dans le formulaire
+            'allow_file_upload' => true,
+            //'photo' => permet de récupérer la photo existante lors d'un update
+            'photo'             => null, 
         ]);
     }
 }
