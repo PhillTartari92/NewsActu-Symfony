cbfbf37099a5da16b451cae5def450c71b768144
PhillTartari92 <phillipevianna@hotmail.fr>
2022-03-17 16:59:31 +0100

    Add articles  to home + create show_article

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,8 +15,12 @@ class DefaultController extends AbstractController
    /**
   * @Route("/", name="default_home", methods={"GET"}) 
   */
-  public function home(): Response
+  public function home(EntityManagerInterface $entityManager): Response
   {
-    return $this->render("default/home.html.twig");
+    $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+
+    return $this->render("default/home.html.twig", [
+        'articles' => $articles,
+    ]);
   }
 }
